24c5afd508afeee0dfe454ed532342bd04119929
Nonlnwza007 <waritmanokaew@gmail.com>
2021-11-26 17:05:18 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int even = 0,odd = 0;
+    int NN = 0,num[NN+1];
+    while(num[NN-1] != 0 or NN == 0){
+            cout << "Enter an integer: ";
+            cin >> num[NN];
+            NN++;
+    }
+    if(num[NN-1] == 0){
+        while(NN !=0){
+        while((num[NN-1]%2) == 0 and NN != 0){
+            even = even +1;
+            NN--;
+            }
+        while((num[NN-1]%2) != 0 and NN != 0){
+            odd = odd + 1;
+            NN--;
+            }
+        }        
+    }
+    cout << "#Even numbers = "  << even-1;
+    cout << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
-}
+}    
+
+
+
